51059784a513e8733316ac0c26f5e145ea919d0b
Amichai <amichaimlevy@gmail.com>
2011-12-13 12:44:02 -0500

    Refactored the solver to eliminate code duplication.

--- Common/ComputationHelper/EqSolver.cs
+++ Common/ComputationHelper/EqSolver.cs
@@ -23,31 +23,19 @@ namespace Common {
 		}
 		public static double NewtonRaphson(this SingleVariableEq function, SingleVariableEq derivative, double approx,
 		 double xMin, double xMax, double eps, int maxiter) {
-			int i = 0;
-			double x1 = approx;
-			double x0;
-			do {
-				x0 = x1;
-				if ((x0 >= xMax) || (x0 <= xMin)) return 0;
-				if (i > maxiter) return 0;
-				x1 = x0 - function.Evaluate(x0) / derivative.Evaluate(x0);
-				i++;
-				Debug.Print(Math.Abs(x1 - x0).ToString() + " " + eps.ToString());
-			} while (Math.Abs(x1 - x0) >= eps);
-			approx = x1;
-			return approx;
+			 return new SingleVariableEq(i => i - function.Evaluate(i) / derivative.Evaluate(i)
+				 ).IterativeSolver(approx, xMin, xMax, eps, maxiter);
 		}
-		public static double Iter(this SingleVariableEq function, double approx, double x0, double xMin, double xMax,
+		public static double IterativeSolver(this SingleVariableEq function, double approx, double xMin, double xMax,
 			double eps, int maxiter) {
 			int i = 0;
-			double x2 = x0;
+			double x2 = approx;
 			double x1;
 			do {
 				x1 = x2;
 				if (x1 >= xMax || x1 <= xMin) return double.MinValue;
 				if (i > maxiter) return double.MinValue;
 				x2 = function.Evaluate(x1);
-				
 				i++;
 			}while (Math.Abs(x2 - x1) >= eps);
 			approx = x2;
