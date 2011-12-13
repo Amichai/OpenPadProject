df0ed28313e91c357bfa29cbec7f605c67124017
Amichai <amichaimlevy@gmail.com>
2011-12-13 10:39:03 -0500

    Working and modular NewtonRaphson method for finding roots. Minor improvements to Graphing.

--- Common/ComputationHelper/EqSolver.cs
+++ Common/ComputationHelper/EqSolver.cs
@@ -9,7 +9,7 @@ using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Common {
 	public static class Solvers {
-		public static double NewtonRalfson(this SingleVariableEq function, double initialApprox, int numberOfIterations, SingleVariableEq derivative) {
+		public static double NewtonRaphson(this SingleVariableEq function, double initialApprox, int numberOfIterations, SingleVariableEq derivative) {
 			double approximation = initialApprox;
 			for (int i = 0; i < numberOfIterations; i++) {
 				Debug.Print(approximation.ToString());
@@ -20,6 +20,23 @@ namespace Common {
 					return approximation;
 			}
 			return approximation;
+		}
+		public static double NewtonRaphson(this SingleVariableEq function, SingleVariableEq derivative, double approx,
+		 double xMin, double xMax, double eps, int maxiter) {
+			int i = 0;
+			double x1 = approx;
+			double x0;
+			do {
+				x0 = x1;
+				if ((x0 >= xMax) || (x0 <= xMin)) return 0;
+				if (i > maxiter) return 0;
+				x1 = x0 - function.Evaluate(x0) / derivative.Evaluate(x0);
+				i++;
+				Debug.Print(Math.Abs(x1 - x0).ToString() + " " + eps.ToString());
+			}
+			while (Math.Abs(x1 - x0) >= eps);
+			approx = x1;
+			return approx;
 		}
 	}
 }
---
+++ Common/ComputationHelper/Graphing/GraphingExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Common.ComputationHelper.Graphing {
+	public static class GraphingExtensionMethods {
+		public static void Graph(this Series ser){
+			if (ser == null)
+				throw new NullReferenceException();
+			new Graph(ser).ShowDialog();
+		}
+	}
+}
