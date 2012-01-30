ffb261e02199645abe8af9f431a35b8dd5ae9a3d
Amichai <amichaimlevy@gmail.com>
2012-01-30 11:47:16 -0500

    Added a RandomNumber generator.

---
+++ Common/Random.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common {
+	public class MyRandom {
+		double mult = 1664525;
+		double add = 1013904223;
+		double m = 2e32;
+		double rn = DateTime.Now.Second;
+		public double NextDouble() {
+			rn = Math.Abs(rn * mult + add) % m;
+			return rn / m;
+		}
+	}
+}
